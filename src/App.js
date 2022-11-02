6e7fccfc2984ed55ba3d28a086951ad0ecefc6f6
dev-rahim <rahimuddin5915@gmail.com>
2022-11-02 18:39:59 +0600

    added signout button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { useState } from 'react'
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, signOut } from 'firebase/auth'
 import './App.css';
 import initializeAuthentication from './Firebase/FirebaseInitialize';
 
-const provider = new GoogleAuthProvider()
+const googleProvider = new GoogleAuthProvider()
+const githubProvider = new GithubAuthProvider();
 
 initializeAuthentication()
 function App() {
+  const [userInfo, setUserInfo] = useState({})
   const handleGoogleSignIn = () => {
     const auth = getAuth()
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then(result => {
-        const user = result.user;
-        console.log(user);
+        const { displayName, email, photoURL, } = result.user;
+        const userDetails = { name: displayName, email: email, img: photoURL };
+        setUserInfo(userDetails);
       })
+  }
+
+  const handleGithubSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, githubProvider)
+      .then(result => {
+        const { displayName, photoURL, } = result.user;
+        const userDetails = { name: displayName, img: photoURL };
+        setUserInfo(userDetails);
+      })
+  }
+
+  const handleSignOut = () => {
+    const auth = getAuth()
+    signOut(auth).then(() => {
+      setUserInfo({})
+    })
   }
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {
+        !userInfo.name ?
+          <div>
+            <button onClick={handleGoogleSignIn}>Google Sign In</button>
+            <button onClick={handleGithubSignIn}>Github Sign In</button>
+          </div> :
+          <button onClick={handleSignOut}>Sign Out</button>}
+      <br /><br />
+      {
+        userInfo.name &&
+        <div>
+          <img src={userInfo.img} alt="" />
+          <h2>Welcome {userInfo.name}</h2>
+          <p>I know your email {userInfo?.email}</p>
+        </div>
+      }
     </div>
   );
 }
