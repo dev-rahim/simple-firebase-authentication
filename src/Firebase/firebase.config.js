bc9bf46ab7933bd04da4ba6ba0ab64dc12865646
dev-rahim <rahimuddin5915@gmail.com>
2022-11-02 10:29:51 +0600

    implement google authentication

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDyP5smLo956IXsqADKtpfuZYXeusjgDUs",
+    authDomain: "simple-firebase-authenti-b0eaa.firebaseapp.com",
+    projectId: "simple-firebase-authenti-b0eaa",
+    storageBucket: "simple-firebase-authenti-b0eaa.appspot.com",
+    messagingSenderId: "908679408486",
+    appId: "1:908679408486:web:2c19585e0280258fb736a7"
+};
+
+export default firebaseConfig;
